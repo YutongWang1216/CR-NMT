1fe93015fab7f644b0ebf83eb8364facefa9b973
Ashton2000 <1764027676@qq.com>
2023-06-27 19:34:15 +0800

    structure modified

--- ent_bertscore/ent_score.py
+++ ent_bertscore/ent_score.py
@@ -1,8 +1,47 @@
-from commonsence_score import cs_score
 import torch
 import numpy as np
 import argparse
 from transformers import AutoTokenizer
+from bert_score.bert_score import score
+
+
+def cs_score(cands, correct_refs, wrong_refs, lang, verbose, device=None, ent_weight=1.4, idf=False, tokenizer=None):
+    correct_mask_dict = None
+    wrong_mask_dict = None
+
+    if tokenizer is None:
+        tokenizer = AutoTokenizer.from_pretrained("roberta-large", use_fast=False)
+
+    if ent_weight != 1.0:
+
+        correct_mask_dict = dict()
+        wrong_mask_dict = dict()
+
+        for correct_ref, wrong_ref in zip(correct_refs, wrong_refs):
+            correct_tokens = tokenizer.encode(correct_ref, add_special_tokens=True, truncation=True)
+            wrong_tokens = tokenizer.encode(wrong_ref, add_special_tokens=True, truncation=True)
+
+            # extract commonsense entities
+            correct_set = set(correct_tokens)
+            wrong_set = set(wrong_tokens)
+
+            correct_ents = correct_set - wrong_set
+            wrong_ents = wrong_set - correct_set
+
+            # build masks for commonsense entities
+            correct_mask = torch.tensor([1 if i in correct_ents else 0 for i in correct_tokens])
+            wrong_mask = torch.tensor([1 if i in wrong_ents else 0 for i in wrong_tokens])
+
+            correct_mask_dict[correct_ref] = correct_mask
+            wrong_mask_dict[wrong_ref] = wrong_mask
+
+    # calculate entity-aware BERTScore
+    correct_scores = score(cands, correct_refs, lang=lang, verbose=verbose, idf=idf, device=device,
+                           ent_dict=correct_mask_dict, ent_weight=ent_weight)
+    wrong_scores = score(cands, wrong_refs, lang=lang, verbose=verbose, idf=idf, device=device,
+                         ent_dict=wrong_mask_dict, ent_weight=ent_weight)
+
+    return correct_scores, wrong_scores
 
 
 def calc_score():
